6fac9f7268befe24d3b102e50365b7d5438ade82
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-13 23:33:22 -0300

    se implementa html y css para la app

---
+++ src/todos/app.js
@@ -0,0 +1,21 @@
+
+// creare - renderizara la aplicación
+// El elementId sera el elemento donde se renderizara la app
+
+
+import html from './app.html?raw'
+
+
+/**
+ * 
+ * @param {String} elementId 
+ */
+export const App = (elementId)=>{
+
+    (()=>{
+        const app = document.createElement('div');
+        app.innerHTML = html
+        document.querySelector(elementId).append(app)
+
+    })();
+}
