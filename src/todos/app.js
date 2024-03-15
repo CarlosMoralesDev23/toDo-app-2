1e45dc8efd2052206749a4154d2c5256a127e3c3
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-15 07:33:17 -0300

    se inplemento funcionalidad de agregar nuevos todos y el input se reincia

--- src/todos/app.js
+++ src/todos/app.js
@@ -9,9 +9,9 @@ import { renderTodos } from './use-cases';
 
 
 const ElementIDs = {
-
-    TodoList:'.todo-list',
-}
+    TodoList: ".todo-list",
+    NewTodoInput: "#new-todo-input",
+};
 
 /**
  * 
@@ -32,4 +32,35 @@ export const App = (elementId)=>{
         document.querySelector(elementId).append(app)
         displayTodos()
     })();
+
+
+    //referencias HTML
+    const newDescriptionInput = document.querySelector( ElementIDs.NewTodoInput )
+
+    // Listeners
+    newDescriptionInput.addEventListener('keyup', (event)=>{
+        //*   1-  keyup  escuchara cada tecla
+                // console.log(event)                     útil para leer los event
+        // console.log(event.target)
+        // console.log(event.target.value)
+
+        //*   2- Mientras la tecla no sea Enter keyCode=13 se seguiran acumulando caracteres
+        if (event.keyCode !== 13) return
+
+
+
+        //*   3- Si luego del "enter"; al aplicar trim, que quita espacios al principo y final, tiene longuitud Cero, la función no hace nada. 
+        if (event.target.value.trim().length === 0) return
+
+
+
+        //*   4- Si la tecla fue Enter y ademas la longuitud fue mayor a Cero,  entonces la función continua con...
+        todoStore.addTodo ( event.target.value );
+        displayTodos();
+        event.target.value = '';
+
+
+    })
+
+
 }
