f80ccc87aef628142cc8531d097402863b9a218b
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-15 06:35:32 -0300

    se implementa el html desde js para crear los todo

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -1,18 +1,26 @@
 import { Todo } from "../models/todo.model";
 
-
 /**
- * 
- * @param {Todo} todo 
+ *
+ * @param {Todo} todo
  */
-export const createTodoHTML = ( todo )=>{
+export const createTodoHTML = (todo) => {
+    if (!todo) throw new Error("A TODO object is required");
 
-    if (!todo) throw new Error('A TODO object is required')
+    const html = `            
+            <li class="${ todo.done && 'checked'}" data-id="${todo.id}">
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${ todo.done && 'checked'}>
+                    <label>Probar JavaScript</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template">
+            </li>`;
 
-    const html = `<h1> ${todo.description} </h1>`
+    const liElement = document.createElement("li");
+    liElement.innerHTML = html;
+    liElement.setAttribute('data-id', todo.id)
+    liElement.classList.add( todo.done && 'completed' )
 
-    const liElement = document.createElement('li')
-    liElement.innerHTML= html
-
-    return liElement
-}
+    return liElement;
+};
